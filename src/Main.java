b7d39eb35f5ff1ca7318ac5199f72ae70d5d1d12
Альвина <alvina-b@yandex.ru>
2022-10-27 14:04:24 +0600

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+byte apple = 4;
+        System.out.println("Значение переменной apple с типом byte равно " + apple);
+int peach = 9;
+        System.out.println("Значение переменной peach с типом int равно " + peach);
+short pear = 15;
+        System.out.println("Значение переменной pear с типом short равно " + pear);
+long berries = 365L;
+        System.out.println("Значение переменной berries с типом long равно " + berries);
+        float workingHours = 10;
+        System.out.println("Значение переменной workingHours с типом float равно " + workingHours);
+        double jam = 15.4;
+        System.out.println("Значение переменной jam с типом double равно " + jam);
+
     }
 }
